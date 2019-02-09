65c77df03975e7ebc0b22a91f9264d42d7213a04
Bruno Carvalhal <b.carvalhal@outlook.com>
2019-02-09 13:47:02 +0100

    Added routes for creating and joining games

--- mancala-spring/src/main/java/com/extrabb/mancala/Game.java
+++ mancala-spring/src/main/java/com/extrabb/mancala/Game.java
@@ -1,19 +1,61 @@
 package com.extrabb.mancala;
 
-public class Game {
-    private final long id;
-    private final String name;
+import java.util.UUID;
 
-    public Game(long id, String name) {
-        this.id = id;
-        this.name = name;
+//---------------------------------------------------//
+//       |     |     |     |     |     |     |       //
+//       |  1  |  2  |  3  |  4  |  5  |  6  |       //
+//       |     |     |     |     |     |     |       //
+//   0   |------------------------------------   7   //
+//       |     |     |     |     |     |     |       //
+//       |  1  |  1  |  1  |  1  |  9  |  8  |       //
+//       |  3  |  2  |  1  |  0  |     |     |       //
+//---------------------------------------------------//
+
+public class Game {
+    // Game Metadata
+    private final String id;
+    private final String player1;
+    private String player2;
+
+    // Game state
+    private int[] board;
+    private String nextPlayer;
+
+    public Game(String player1) {
+        this.id = UUID.randomUUID().toString();
+        this.player1 = player1;
     }
 
-    public long getId() {
+    private void initializeGame() {
+        this.board = new int[14];
+        for(int i = 0; i < 14; i++) {
+            if(i % 7 == 0) {
+                continue;
+            }
+            board[i] = 6;
+        }
+        this.nextPlayer = player1;
+    }
+
+    public boolean joinGame(String player2) {
+        if(this.player2 != null) {
+            return false;
+        }
+        this.player2 = player2;
+        this.initializeGame();
+        return true;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public int[] getBoard() {
+        return board;
+    }
+
+    public String nextPlayer() {
+        return nextPlayer;
     }
 }
--- mancala-spring/src/main/java/com/extrabb/mancala/GameController.java
+++ mancala-spring/src/main/java/com/extrabb/mancala/GameController.java
@@ -1,18 +1,56 @@
 package com.extrabb.mancala;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class GameController {
 
-    private final AtomicLong counter = new AtomicLong();
+    private Map<String, Game> activeGames = new HashMap<String, Game>();
 
     @RequestMapping(path="/game", method= RequestMethod.GET)
-    public Game game(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Game(counter.incrementAndGet(), name);
+    public ResponseEntity getGame(@RequestParam(value="id") String id) {
+        if(activeGames.containsKey(id)) {
+            return ResponseEntity.status(HttpStatus.OK).body(activeGames.get(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Game was not found");
+        }
+    }
+
+    @RequestMapping(path="/games", method= RequestMethod.GET)
+    public ResponseEntity getGames() {
+        List<String> gameIds = activeGames.values().stream().map(game -> game.getId()).collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(gameIds);
+    }
+
+    @RequestMapping(path="/game", method= RequestMethod.PUT)
+    public Game createGame(HttpServletRequest request) {
+        Game game = new Game(request.getSession().getId());
+        activeGames.put(game.getId(), game);
+        return game;
+    }
+
+    @RequestMapping(path="/game/join", method= RequestMethod.POST)
+    public ResponseEntity createGame(@RequestParam(value="id") String id, HttpServletRequest request) {
+        if(activeGames.containsKey(id)) {
+            Game game = activeGames.get(id);
+            boolean result = game.joinGame(request.getSession().getId());
+            if(result) {
+                return ResponseEntity.status(HttpStatus.OK).body(game);
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Game already in progress");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Game was not found");
+        }
     }
 }
