ebafb2072d63618517be5421d1bed7bb6b99e81c
Bruno Carvalhal <b.carvalhal@outlook.com>
2019-02-10 14:17:03 +0100

    Added capturing of stones

--- mancala-spring/src/main/java/com/extrabb/mancala/Board.java
+++ mancala-spring/src/main/java/com/extrabb/mancala/Board.java
@@ -19,6 +19,11 @@ public class Board {
     private final int BIG_PIT_2;
 
     private int[] pockets;
+    private boolean finished = false;
+
+    public int[] getPockets() {
+        return pockets;
+    }
 
     public Board(int size, int startingPieces) {
         if(size <= 0 || size % 2 != 0) {
@@ -53,7 +58,7 @@ public class Board {
     }
 
     public int makeMove(int player, int pocket) {
-        if(!isLegalMove(player, pocket)) {
+        if(finished || !isLegalMove(player, pocket)) {
             return 0;
         }
 
@@ -71,6 +76,9 @@ public class Board {
             stones--;
         }
 
+        // Capture stones
+        this.tryCaptureStones(nextPocket, player);
+
         // Player can go again if he finishes in his own big pit
         if(nextPocket == BIG_PIT_1 || nextPocket == BIG_PIT_2) {
             return player;
@@ -79,18 +87,37 @@ public class Board {
         }
     }
 
-    public int[] getPockets() {
-        return pockets;
+    private boolean pocketBelongsToPlayer(int pocket, int player) {
+        if(player == 1) {
+            return pocket > 0 && pocket < BOARD_SIZE / 2 - 1;
+        } else {
+            return pocket > BOARD_SIZE / 2 - 1 && pocket < BOARD_SIZE - 1;
+        }
+    }
+
+    private void tryCaptureStones(int lastPocket, int player) {
+        if(this.pockets[lastPocket] == 1 && this.pocketBelongsToPlayer(lastPocket, player)) {
+            int oppositePit = lastPocket + 2 * (BOARD_SIZE / 2 - lastPocket - 1);
+            this.pockets[lastPocket] += this.pockets[oppositePit];
+            this.pockets[oppositePit] = 0;
+        }
     }
 
     public String determineGameStatus() {
         int player1Sum = Arrays.stream(this.pockets).limit(BOARD_SIZE / 2 - 1).sum();
         int player2Sum = Arrays.stream(this.pockets).skip(BOARD_SIZE / 2).limit(BOARD_SIZE / 2 - 1).sum();
 
-        if(player1Sum == 0) {
-            return String.format("Player 1 won with %d stones in his big pit", this.pockets[BOARD_SIZE / 2 - 1]);
-        } else if(player2Sum == 0) {
-            return String.format("Player 2 won with %d stones in his big pit", this.pockets[BOARD_SIZE - 1]);
+        if(player1Sum == 0 || player2Sum == 0) {
+            finished = true;
+            int player1Score = this.pockets[BOARD_SIZE / 2 - 1] + player1Sum;
+            int player2Score = this.pockets[BOARD_SIZE - 1] + player2Sum;
+            if(player1Score > player2Score) {
+                return String.format("Player 1 won with %d stones in his big pit", player1Score);
+            } else if (player1Score < player2Score) {
+                return String.format("Player 2 won with %d stones in his big pit", player2Score);
+            } else {
+                return "It's a draw!";
+            }
         } else {
             return "Playing";
         }
